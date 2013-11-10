e3401e9a2eaebed9a22839396b1be172f43a4249
guoquan <guoquan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-10 11:44:03 +0000

    update readme.txt for mistakes
    git-svn-id: https://plugins.svn.wordpress.org/summary-page/trunk@801967 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== Summary Page ===
+Contributors: guoquan
+Donate link: http://guoquan.org
+Tags: child pages, meta
+Requires at least: 3.7.1
+Tested up to: 3.7.1
+Stable tag: 0.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+List and/or include sub-pages with meta option.
+
+== Description ==
+
+List and/or include sub-pages with meta option.
+
+After install and activate the plugin, add a custom field 'is_summary' and set to 1 to enable for each individual page.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload `summary-page.zip` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add a custom field 'is_summary' and set to 1 to enable for each individual page.
+
+== Frequently Asked Questions ==
+
+
+== Screenshots ==
---
+++ summary-page.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+* Plugin Name: Summary Page
+* Plugin URI: https://github.com/guoquan/summary-page
+* Description: List and/or include sub-pages with meta option.
+* Version: 0.0.1
+* Author: Guo Quan
+* Author URI: http://guoquan.org
+*/
+
+//[summary-child-pages] 
+//[summary-child-pages id=parent_id]
+
+
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+// define once
+if ( !class_exists( 'SummaryPage' ) ) :
+
+// 
+class SummaryPage
+{
+	public static $IS_SUMMARY_KEY = 'is_summary';
+
+	// Singleton 
+	private static $instance;
+	public static function instance() {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new SummaryPage();
+
+			self::$instance->setup_component();
+		}
+		return self::$instance;
+	}
+
+	private function __construct() {
+		// and absolutly nothing here!!!
+		// turn to instance function
+	}
+
+	/**
+	 * A dummy magic method to prevent singleton instanse from being cloned
+	 *
+	 */
+	public function __clone() { _doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'SummaryPage' ), '1.7' ); }
+
+	/**
+	 * A dummy magic method to prevent singleton instanse from being unserialized
+	 *
+	 */
+	public function __wakeup() { _doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'SummaryPage' ), '1.7' ); }
+
+
+	private function setup_component() {
+		add_action( 'pre_get_posts', array($this,'show_child') );
+	}
+
+
+	public function show_child( $query ) {
+	    if ( $query->is_page() && $query->is_main_query() ) {
+	    	//print_r($query);
+
+	    	$page_id = $query->get_queried_object_id();
+	    	if ( !$page_id ) $page_id = $query->get( 'page_id' ); // in case referenced by index
+			
+	    	$is_summary = get_post_meta( $page_id, self::$IS_SUMMARY_KEY, true );
+	    	if ( $is_summary ) {
+	    		$children = get_pages( array(
+					'parent'		=> $page_id,
+					'post_type'		=> 'page',
+					'sort_column'	=> 'menu_order',
+					'sort_order'	=> 'ASC',
+				));
+
+		        $incl = array((int)$page_id);
+		        foreach ($children as $key => $value) {
+		        	array_push( $incl, (int)($value->ID) );
+		        }
+		        //print_r($incl);
+
+		    	$query->init();
+		    	$query->set( 'order',			'ASC' );
+		    	$query->set( 'orderby',			'post__in' );
+		    	$query->set( 'posts_per_page',	'-1' );
+		    	$query->set( 'post_type',		'page' );
+		    	$query->set( 'post__in',		$incl );
+	    	}	
+	    }
+	    return;
+	}
+}
+
+function sp() {
+	return SummaryPage::instance();
+}
+
+$sp = sp();
+
+endif;
+?>
